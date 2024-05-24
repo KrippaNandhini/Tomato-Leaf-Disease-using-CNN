727d480f825d1c4b6c24a3766e75dc4240d72fa0
KrippaNandhini <79848429+KrippaNandhini@users.noreply.github.com>
2024-05-24 20:51:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense,BatchNormalization, Dropout
+from keras.initializers import RandomNormal
 from tensorflow.keras.models import load_model
 from PIL import Image
 import numpy as np
